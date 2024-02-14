004fc670b0acb32a70e50009cd26ec25424ee376
vuvuzelich <louielamani@ukr.net>
2024-02-14 15:48:53 +0200

    new blocks is itemsBanks

---
+++ FinTechOOPBasedMobilePayrollSystemEU/Views/ChooseBankView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseBankView.swift
+//  FinTechOOPBasedMobilePayrollSystemEU
+//
+//  Created by Макс Лахман on 13.02.2024.
+//
+
+import SwiftUI
+
+struct ChooseBankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseBankView()
+}
