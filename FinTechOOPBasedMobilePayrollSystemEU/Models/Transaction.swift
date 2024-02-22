043fa597aa9643b5136eff18fb89f3adbf1a8486
vuvuzelich <louielamani@ukr.net>
2024-02-22 16:56:04 +0200

    add new functionality

---
+++ FinTechOOPBasedMobilePayrollSystemEU/Models/Transaction.swift
@@ -0,0 +1,8 @@
+//
+//  Transaction.swift
+//  FinTechOOPBasedMobilePayrollSystemEU
+//
+//  Created by Макс Лахман on 22.02.2024.
+//
+
+import Foundation
---
+++ FinTechOOPBasedMobilePayrollSystemEU/Models/TransactionsGroup.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionsGroup.swift
+//  FinTechOOPBasedMobilePayrollSystemEU
+//
+//  Created by Макс Лахман on 22.02.2024.
+//
+
+import Foundation
