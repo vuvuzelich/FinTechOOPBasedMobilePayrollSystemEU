aea401ac0f4534d820a12f1feb50ae96757aca96
vuvuzelich <louielamani@ukr.net>
2024-02-22 12:36:19 +0200

    add funk and send.view

---
+++ FinTechOOPBasedMobilePayrollSystemEU/Views/BuyView.swift
@@ -0,0 +1,18 @@
+//
+//  BuyView.swift
+//  FinTechOOPBasedMobilePayrollSystemEU
+//
+//  Created by Макс Лахман on 19.02.2024.
+//
+
+import SwiftUI
+
+struct BuyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuyView()
+}
---
+++ FinTechOOPBasedMobilePayrollSystemEU/Views/ExchangeView.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeView.swift
+//  FinTechOOPBasedMobilePayrollSystemEU
+//
+//  Created by Макс Лахман on 19.02.2024.
+//
+
+import SwiftUI
+
+struct ExchangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeView()
+}
